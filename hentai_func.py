8fbda6f6ac4bc63d24235d1e6bc3c411a41e3f6d
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-07-28 00:13:41 +0200

    Create hentai_func.py

---
+++ hentai_func.py
@@ -0,0 +1,96 @@
+from enum import Enum
+from random import choice, shuffle
+from typing import Optional
+from aiohttp import ClientSession
+from requests import get
+
+
+class NsfwApis(Enum):
+    Rule43Api="https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit=500&tags=score:>10+rating:explicit+"
+    KonachanApi = "https://konachan.com/post.json?s=post&q=index&limit=100&tags=score:>10+rating:explicit+"
+    YandereApi = "https://yande.re/post.json?limit=100&tags=score:>10+rating:explicit+"
+    GelbooruApi = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=score:>10+rating:explicit+"
+    RandomApi = [Rule43Api, KonachanApi, YandereApi, GelbooruApi]
+
+class NekosFunTags(Enum):
+    anal="anal"
+    blowjob="bj"
+    cum="cum"
+    hentai="hentai"
+    yuri="lesbian"
+
+class Hentai:
+    def __init__(self):
+        self.blacklisted_tags = {"loli", "shota", "cub"}
+
+    def format_tags(self, tags: str = None):
+        if tags:
+            tags = [
+                tag.strip().replace(" ", "_")
+                for tag in tags.split(",")
+                if tag.strip().replace(" ", "_")
+            ]
+            tags_string = "+".join(tags)
+            return tags_string
+        else:
+            return ""
+
+    async def get_nsfw_image(self, provider: NsfwApis, tags: Optional[str] = None):
+        if provider == NsfwApis.RandomApi:
+            provider = choice(NsfwApis.RandomApi)
+
+        tags = tags.lower() if tags else None
+
+        url = provider.value + self.format_tags(tags)
+
+        async with ClientSession() as session:
+            async with session.get(url) as resp:
+                nsfw_images = await resp.json()
+
+        if not nsfw_images:
+            return None
+
+        if provider.value == provider.GelbooruApi.value:
+            nsfw_images_list = list(nsfw_images.get("post", []))
+        else:
+            nsfw_images_list = list(nsfw_images)
+
+        shuffle(nsfw_images_list)
+
+        if not tags:
+            tags = ""
+
+        tags_list = [
+            tag.strip().replace(" ", "_")
+            for tag in tags.split(",")
+            if tag.strip().replace(" ", "_") not in self.blacklisted_tags
+        ]
+
+        if len(tags_list) == 0 or len(tags_list) > 3:
+            return None
+
+        filtered_ret = [
+            img
+            for img in nsfw_images_list
+            if all(tag in img["tags"] for tag in tags_list)
+        ]
+
+        if len(filtered_ret) == 0:
+            return None
+
+        filtered_images = []
+        for image in filtered_ret:
+            tags = image["tags"].lower().split(" ")
+            if any(tag in self.blacklisted_tags for tag in tags):
+                continue
+            filtered_images.append(image)
+        return choice(filtered_images)
+
+    @staticmethod
+    def nekofun(endpoint:str):
+        r = get("http://api.nekos.fun:8080/api/" + endpoint)
+        if r.status_code != 200:
+            return "An error has occurred"
+        else:
+            return str(r.json()["url"])
+
